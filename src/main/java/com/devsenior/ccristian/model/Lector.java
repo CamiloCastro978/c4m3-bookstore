c7c1982cf5c84f0063c0384d1a9ffcec92d8f6bd
Cristian Camilo Castro <castrocristiancamilo9@gmail.com>
2025-08-14 08:29:05 -0500

    Model classes added

--- src/main/java/com/devsenior/ccristian/model/Lector.java
+++ src/main/java/com/devsenior/ccristian/model/Lector.java
@@ -3,7 +3,33 @@ package com.devsenior.ccristian.model;
 import java.time.LocalDate;
 
 public class Lector {
+
     private String nombre;
     private LocalDate fechaMulta;
-    private Prestamo [] prestamos;
+    private Prestamo[] prestamos;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaMulta() {
+        return fechaMulta;
+    }
+
+    public void setFechaMulta(LocalDate fechaMulta) {
+        this.fechaMulta = fechaMulta;
+    }
+
+    public Prestamo[] getPrestamos() {
+        return prestamos;
+    }
+
+    public void setPrestamos(Prestamo[] prestamos) {
+        this.prestamos = prestamos;
+    }
+
 }
